9a5cc223eb46f99f0b40ca889922a148e8f1e718
StarFenix <starfenix66@gmail.com>
2021-01-27 22:05:22 -0600

    Agregando Auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'GymNew';
+
+  constructor(public auth: AngularFireAuth){
+    this.auth.user.subscribe((usuario)=>{
+      console.log(usuario)
+    })
+  }
+  login() {
+    this.auth.signInWithEmailAndPassword("starfenix66@gmail.com", "123456789")
+  }
+  logout() {
+    this.auth.signOut();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from 'src/environments/environment';
+import { AngularFireAuth } from '@angular/fire/auth';
 @NgModule({
   declarations: [
     AppComponent
@@ -17,7 +18,9 @@ import { environment } from 'src/environments/environment';
     BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase)
   ],
-  providers: [],
+  providers: [
+    AngularFireAuth
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
