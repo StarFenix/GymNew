684461de8e36dfe438781e5db88c661e42b5ba5f
StarFenix <starfenix66@gmail.com>
2021-01-28 20:38:00 -0600

    Validando el login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,9 +9,14 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   formularioLogin!: FormGroup;
-  constructor(private creadorFormulario: FormBuilder, public auth: AngularFireAuth) { }
+  datosCorrectos: boolean = true;
+  textoError: string = '';
+  constructor(public creadorFormulario: FormBuilder, public auth: AngularFireAuth) { }
 
   ngOnInit(): void {
+    this.formulario();
+  }
+  formulario(){
     this.formularioLogin = this.creadorFormulario.group({
       email: ['', Validators.compose([
         Validators.required, Validators.email
@@ -21,8 +26,18 @@ export class LoginComponent implements OnInit {
   }
   ingresar()
   {
-    this.auth.signInWithEmailAndPassword(this.formularioLogin.value.email, this.formularioLogin.value.password).then((usuario)=>{
-      console.log(usuario)
-    })
+    if(this.formularioLogin.valid){
+      this.datosCorrectos = true;
+      this.auth.signInWithEmailAndPassword(this.formularioLogin.value.email, this.formularioLogin.value.password).then((usuario)=>{
+        console.log(usuario)
+      }).catch((error)=>{
+        this.datosCorrectos = false;
+        this.textoError = error.message;
+      })
+    }
+    else{
+      this.textoError = 'Verifique que los datos ingresados sean correctos'
+      this.datosCorrectos = false;
+    }
   }
 }
