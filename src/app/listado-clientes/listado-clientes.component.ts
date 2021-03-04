2124406ebad04cf6fe1ff42cefd7f26b43416ff4
StarFenix <starfenix66@gmail.com>
2021-03-03 19:22:26 -0600

    Leer ID del cliente

--- src/app/listado-clientes/listado-clientes.component.ts
+++ src/app/listado-clientes/listado-clientes.component.ts
@@ -1,17 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+
 @Component({
   selector: 'app-listado-clientes',
   templateUrl: './listado-clientes.component.html',
   styleUrls: ['./listado-clientes.component.scss']
 })
 export class ListadoClientesComponent implements OnInit {
-  cliente: any[] = new Array<any>();
+  clientes: any[] = new Array<AngularFirestore>();
   constructor(private db: AngularFirestore ) { }
 
   ngOnInit(): void {
-    this.db.collection('clientes').valueChanges().subscribe((resultado)=>{
-      this.cliente = resultado;
+    /* this.db.collection('clientes').valueChanges().subscribe((resultado)=>{
+      this.clientes = resultado;
+      console.log(resultado)
+    }); */
+    this.clientes.length = 0;
+    this.db.collection('clientes').get().subscribe((resultado) =>{
+      console.log(resultado.docs)
+
+      //Opcion 1
+      resultado.docs.forEach((item: any)=>{
+        let cliente = item.data();
+        cliente.id = item.id;
+        cliente.ref = item.ref;
+        this.clientes.push(cliente)
+      })
+      //Opción 2
+     /*  for(let item of resultado.docs){
+        console.log(item.data())
+        console.log(item.ref)
+      } */
     })
   }
 
