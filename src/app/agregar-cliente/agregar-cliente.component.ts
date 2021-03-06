e01ee62df70a18dfde5d6403e57e3e71d9ae44c4
StarFenix <starfenix66@gmail.com>
2021-03-05 19:19:10 -0600

    Validar Formulario

--- src/app/agregar-cliente/agregar-cliente.component.ts
+++ src/app/agregar-cliente/agregar-cliente.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar-cliente',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agregar-cliente.component.scss']
 })
 export class AgregarClienteComponent implements OnInit {
-
-  constructor() { }
+formularioCliente!: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formularioCliente = this.fb.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      email: ['', Validators.compose([
+        Validators.required, Validators.email
+      ])],
+      fcNacimiento: ['', Validators.required],
+      telefono: [''],
+      img: ['', Validators.required]
+    })
+  }
+
+  agregar(){
+    console.log(this.formularioCliente.value)
   }
 
 }
