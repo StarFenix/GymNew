6cacf4f48f7c80ecdf20ef7477f240dd6c451e7f
StarFenix <starfenix66@gmail.com>
2021-03-13 18:00:27 -0600

    Maquetar Inscripcion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AgregarClienteComponent } from './agregar-cliente/agregar-cliente.component';
+import { InscripcionComponent } from './inscripcion/inscripcion.component';
 import { ListadoClientesComponent } from './listado-clientes/listado-clientes.component';
 import { PreciosComponent } from './precios/precios.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'inscripcion', pathMatch: 'full'},
+  {path: 'inscripcion', component: InscripcionComponent},
   {path: 'listaClientes', component: ListadoClientesComponent},
   {path: 'agregarCliente', component: AgregarClienteComponent},
   {path: 'agregarCliente/:clienteID', component: AgregarClienteComponent},
